e903056d5a146240349e01ece9f4ce67afc2163a
Kelvin Maina <thiongokelvin5@gmail.com>
2020-04-30 16:02:10 +0300

    Factory and Seed

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Model\Question;
+use App\Model\Reply;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,5 +42,10 @@ class User extends Authenticatable
     public function questions()
     {
         return $this->hasMany(Question::class);
+    }
+
+    public function reply()
+    {
+        return $this->hasMany(Reply::class);
     }
 }
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,11 +2,22 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Category;
 use App\Model\Question;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'body' => $faker->text,
+        'slug' => str_replace(' ', '-', $title), //str_slug($title),
+        'category_id' => function(){
+            return Category::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        },
     ];
 });
